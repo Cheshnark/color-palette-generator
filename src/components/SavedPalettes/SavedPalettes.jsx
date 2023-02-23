7245925b37965bd40f17dda41b2e166f095c392c
csnark <csnark.dev@gmail.com>
2023-02-23 09:53:01 +0100

    SavedPalettes component completed

--- src/components/SavedPalettes/SavedPalettes.jsx
+++ src/components/SavedPalettes/SavedPalettes.jsx
@@ -1,9 +1,56 @@
 import './SavedPalettes.css'
 
+import trash from '../../svg/trash.svg'
+
 const SavedPalettes = () => {
+    const palettes = [
+        {
+            id:123456,
+            name:"Jawi",
+            colors: [
+                "#d905ca",
+                "#62c902",
+                "#e386cf",
+                "#05bc3e",
+                "#bbd8a4"
+            ]
+        }
+    ]
+
+    const deletePalette = (e) => {
+        localStorage.removeItem(e.target.id)
+    }
+
     return (
         <section className='saved-palettes'>
-            <h2>Saved</h2>
+            <h2>Saved palettes</h2>
+            <div className="saved-palettes-container">
+                {palettes &&
+                    palettes.map((palette, i) => {
+                    return (
+                        <div className="palette" key={i}>
+                            <div className="palette-name-container">
+                                <h4>{palette.name}</h4>
+                                <img 
+                                    src={trash} 
+                                    alt="trash-icon"
+                                    id={palette.id}
+                                    onClick={deletePalette} />
+                            </div>
+                            <figure className="palette-colors">
+                                {palette.colors.map(color => {
+                                    return(
+                                        <div 
+                                            className="color"
+                                            style={{backgroundColor: `${color}`}} ></div>
+                                    )
+                                })}
+                            </figure>
+                        </div>
+                       
+                    )
+                })}
+            </div>
         </section>
     )
 }
